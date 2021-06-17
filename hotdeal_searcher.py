1cf51fd7e1732ae85e7433b02d7532997c6e201d
JongHyun-GD <58730856+JongHyun-GD@users.noreply.github.com>
2021-06-17 22:05:39 +0900

    init

---
+++ hotdeal_searcher.py
@@ -0,0 +1,26 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import time
+
+site_url = "https://quasarzone.com/bbs/qb_saleinfo"
+latest_title = ""
+
+while True:
+	html = urlopen(site_url)
+	bsObject = BeautifulSoup(html, "html.parser")
+	titles = bsObject.find_all('span', {'class':'ellipsis-with-reply-cnt'})
+	
+	# 업데이트 내용이 있다면 그 내용까지 출력한다.
+	if latest_title != titles[0].text:
+		print("<--- Update detected --->")
+		for t in titles:
+			if t.text != latest_title:
+				print(t.text)
+			else:
+				break
+		latest_title = titles[0].text # 현재 시점에서 최신 게시물이름을 저장한다.
+		print("=====================================")
+	else:
+		print("<--- No update in", site_url, "--->")
+	time.sleep(300)
+
